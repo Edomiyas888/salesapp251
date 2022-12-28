ad75008fe751d93047b4972395bfdeed5cca71c5
Kirubel Adugna <kirubel4375@gmail.com>
2022-12-28 11:06:53 +0300

    changes

--- lib/presenatiation_layer/Screens/homepage.dart
+++ lib/presenatiation_layer/Screens/homepage.dart
@@ -6,8 +6,6 @@ import 'package:salesapp251/functions/methods.dart';
 import 'package:salesapp251/models/buildingModel.dart';
 import 'package:fl_chart/fl_chart.dart';
 
-import '../../Widgets/chart_container.dart';
-
 class FrontPage extends StatefulWidget {
   const FrontPage({super.key, required this.email});
   final String email;
@@ -19,53 +17,78 @@ class FrontPage extends StatefulWidget {
 class _FrontPageState extends State<FrontPage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          height: MediaQuery.of(context).size.height * .4,
-          child: FutureBuilder(
-            future: fetchTask(widget.email),
-            builder: ((BuildContext context,
-                AsyncSnapshot<List<Building>> snapshot) {
-              List<Building>? buildingList = snapshot.data;
-              if (buildingList != null) {
-                return ListView.builder(
-                  itemCount: buildingList.length,
-                  itemBuilder: ((context, index) {
-                    return Container(
-                      height: 200.0,
-                      width: MediaQuery.of(context).size.width * .9,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(40.0),
-                      ),
-                      child: Card(
-                        child: Column(
+    return FutureBuilder(
+      future: fetchTask(widget.email),
+      builder: ((BuildContext context,
+          AsyncSnapshot<List<Building>> snapshot) {
+        List<Building>? buildingList = snapshot.data;
+        if (buildingList != null) {
+          return ListView.builder(
+            itemCount: buildingList.length,
+            itemBuilder: ((context, index) {
+              return Container(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 26.0, vertical: 11.0),
+                height: 110.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(40.0),
+                ),
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 11.0),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment:
+                              MainAxisAlignment.spaceEvenly,
                           children: [
-                            Text(buildingList[index].blgName),
-                            Text(buildingList[index].location),
+                            const Text(
+                              "Building:",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 18.0,
+                              ),
+                            ),
+                            Text(
+                              buildingList[index].blgName,
+                              style: const TextStyle(
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
                           ],
                         ),
-                      ),
-                    );
-                  }),
-                );
-              }
-              return const SizedBox.shrink();
+                        const SizedBox(height: 13.0),
+                        Row(
+                          mainAxisAlignment:
+                              MainAxisAlignment.spaceEvenly,
+                          children: [
+                            const Text(
+                              "Location:",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 18.0,
+                              ),
+                            ),
+                            Text(
+                              buildingList[index].location,
+                              style: const TextStyle(
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              );
             }),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            width: 400,
-            height: 400,
-            child: ChartContainer(
-                title: 'Bar Chart',
-                color: Color(0xfffc5185),
-                chart: BarChartContent()),
-          ),
-        ),
-      ],
+          );
+        }
+        return const SizedBox.shrink();
+      }),
     );
   }
 }
